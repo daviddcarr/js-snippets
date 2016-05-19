7f8ef9d12cb082253216f8b2fbbe72afd42b2e23
David Carr <david@yellow7.com>
2016-05-19 08:53:40 -0500

    shortened random number array name in randomizeList.js

--- randomizeList.js
+++ randomizeList.js
@@ -1,14 +1,14 @@
 //Function to restricted list of numbers from a larger list of numbers
 //Length is the number of numbers, maxVal and minVal tells the code the highest and lowest values it can choose from.
 function randList(length, maxVal, minVal) {
-    var rands = [];
-    while (rands.length < length) {
+    var r = [];
+    while (r.length < length) {
         var rand = Math.floor((Math.random() * maxVal) + minVal),
             used = false;
-        $.each(rands, function(i){
-            if (rand == rands[i]) {used = true;}
+        $.each(r, function(i){
+            if (rand == r[i]) {used = true;}
         });
-        if (!used) {rands.push(rand);}
+        if (!used) {r.push(rand);}
     }
-    return rands;
+    return r;
 }
